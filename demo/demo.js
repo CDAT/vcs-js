94a82758533801c3afb7f8bd2dcc94fb86ba5afd
Sam Fries <fries2@llnl.gov>
2016-10-14 13:56:35 -0700

    Finished refactoring to support deployment changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,23 +1,9 @@
 var sessionPromise;
 
-$(function () {
-  // Ordinarily this would be a URL string of a rest
-  // interface for generating a new server side connection.
-  // Here we short circuit that code to generate a simulated
-  // session connecting to pregenerated files through
-  // Girder's rest interface.
-  var url = 'ws://localhost:9000/ws';
+(function () {
 
   var variables = {
-    "clt": {
-      "id": "clt",
-      "derivation":
-      [
-        {"type": "file", "uri": "clt.nc"},
-        {"parents": [0], "operation": {"type": "get", "id": "clt"}, "type": "variable"},
-        {"parents": [1], "operation": {"squeeze": 0, "type": "subset", "axes": {"longitude": [-180, 180], "latitude": [-90, 90]}}, "type": "variable"}
-      ]
-    },
+    "clt": {"id": "clt", "derivation": [{"type": "file", "uri": "clt.nc"}, {"parents": [0], "operation": {"type": "get", "id": "clt"}, "type": "variable"}]},
     "u": {"id": "u", "derivation": [{"type": "file", "uri": "clt.nc"}, {"parents": [0], "operation": {"type": "get", "id": "u"}, "type": "variable"}]},
     "v": {"id": "v", "derivation": [{"type": "file", "uri": "clt.nc"}, {"parents": [0], "operation": {"type": "get", "id": "v"}, "type": "variable"}]}
   }
@@ -27,92 +13,24 @@ $(function () {
   var vector_subview = {"datawc_timeunits": "days since 2000", "projection": "linear", "reference": 1e+20, "xticlabels1": "*", "xticlabels2": "*", "linecolor": null, "ymtics1": "", "ymtics2": "", "linewidth": null, "datawc_x1": 60, "datawc_x2": 180, "xmtics1": "", "xmtics2": "", "datawc_calendar": 135441, "alignment": "center", "type": "arrows", "colormap": null, "xaxisconvert": "linear", "scale": 1.0, "linetype": null, "datawc_y2": 90, "datawc_y1": 0, "yaxisconvert": "linear", "name": "subset_vector", "yticlabels1": "*", "yticlabels2": "*", "scalerange": [0.1, 1.0], "scaleoptions": ["off", "constant", "normalize", "linear", "constantNNormalize", "constantNLinear"], "g_name": "Gv", "scaletype": "constantNNormalize"};
 
   // create the session
-  sessionPromise = vcs.createSession(url);
-
-  sessionPromise.catch(() => {
-    console.log('Could not connect to ' + url);
-  });
-
-  var canvasPromise = sessionPromise.then(function (session) {
-    return session.init(document.getElementById('vcs-isofill'));
-  });
-
-  // generate the plot when all of the promises resolve
-  canvasPromise.then(function (canvas) {
-    var dataSpec = variables.clt;
-    canvas.plot(dataSpec, 'no_legend', boxfill, 'server');
-  });
-
-   // generate another plot using client side rendering
-  // we don't have an api for getting data yet, so we'll
-  // just use an ajax request to data.kitware.com
-  var cltPromise = $.ajax('https://data.kitware.com/api/v1/file/576aa3c08d777f1ecd6701ae/download');
-  var latPromise = $.ajax('https://data.kitware.com/api/v1/item/576aa3c08d777f1ecd6701b0/download');
-  var lonPromise = $.ajax('https://data.kitware.com/api/v1/item/576aa3c08d777f1ecd6701b9/download');
-  var canvasPromise2 = sessionPromise.then(function (session) {
-    return session.init(document.getElementById('plotly-isofill'));
-  });
+  var canvas = vcs.init(document.getElementById('vcs-boxfill'));
+  var dataSpec = variables.clt;
+  canvas.plot(dataSpec, 'no_legend', boxfill, 'server');
 
 
-  // This is all very rough, likely much of this should be wrapped inside the api
-  Promise.all([
-    canvasPromise2, cltPromise, latPromise, lonPromise
-  ]).then(function (arg) {
-    console.log("1")
-    var canvas = arg[0];
-    console.log("2")
-    var clt = arg[1];
-    console.log("3")
-    var lat = arg[2];
-    console.log("4")
-    var lon = arg[3];
-    console.log("5")
-    var timestep = 0;
-    console.log("6")
-
-    var data = {
-      x: lon.data,
-      y: lat.data
-    };
-    console.log("7")
-
-    function draw() {
-      console.log("8")
-      data.z = ndarray(clt.data, clt.shape).pick(timestep, null, null);
-      console.log("9")
-      canvas.plot(data, 'default', { "g_type": "Gfi" }, 'client');
-      console.log("10")
-      timestep = (timestep + 1) % clt.shape[0];
-    }
-    console.log("11")
-    // call again for the next time step
-    draw();
-  });
-
-  var canvasPromise3 = sessionPromise.then(function (session) {
-    return session.init(document.getElementById('vcs-vector'));
-  });
+  var canvas3 = vcs.init(document.getElementById('vcs-vector'));
     // generate the plot when all of the promises resolve
-  canvasPromise3.then(function (canvas) {
-    var dataSpec = [variables.u, variables.v];
-    canvas.plot(dataSpec, 'default', vector, 'server');
-  });
+  var dataSpec = [variables.u, variables.v];
+  canvas3.plot(dataSpec, 'default', vector, 'server');
 
-  var canvasPromise4 = sessionPromise.then(function (session) {
-    return session.init(document.getElementById('vcs-vector-subset'));
-  });
-
-  // generate the plot when all of the promises resolve
-  canvasPromise4.then(function (canvas) {
-    var dataSpec = [variables.u, variables.v];
-    canvas.plot(dataSpec, 'default', vector_subview, 'server');
-  });
+  var canvas4 = vcs.init(document.getElementById('vcs-vector-subset'));
+  var dataSpec = [variables.u, variables.v];
+  canvas4.plot(dataSpec, 'default', vector_subview, 'server');
 
   $(window).on('beforeunload', function() {
-    canvasPromise.then((canvas) => canvas.close());
-    canvasPromise3.then((canvas) => canvas.close());
-    canvasPromise4.then((canvas) => canvas.close());
+    canvas.close();
+    canvas3.close();
+    canvas4.close();
     return 'Your own message goes here...';
   });
-});
-
+})();
