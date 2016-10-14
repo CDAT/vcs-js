7e6bf4b04369ee61b65085cd1670950bab1e004c
Sam Fries <fries2@llnl.gov>
2016-10-14 11:46:59 -0700

    Simplify deployment, build JS into a directory that is served from the server to allow for automatic URL fill-in

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import glob
+import subprocess
+import os
+
+
+# Build the JS so vcs.js is ready for packaging
+subprocess.check_call("npm run build", shell=True)
+
+
+setup(
+    name="vcs_server",
+    version="0.1",
+    packages=find_packages(),
+    scripts=glob.glob("scripts/*"),
+    package_data={"vcs_server": ["js/vcs.js"]}
+)
