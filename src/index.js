7409bd45b516442c54dbe3b196dce684edd4bdc5
Sam Fries <fries2@llnl.gov>
2016-10-14 13:56:35 -0700

    Finished refactoring to support deployment changes

--- src/index.js
+++ src/index.js
@@ -7,35 +7,33 @@ import plotly from './plotly';
 
 const clients = {};
 
-const obj = {
-  init: (el) => {
-    const canvas = {
-      el,
-      plot(dataSpec, template, method, renderingType) {
-        switch (renderingType) {
-          case 'client': {
-            return plotly.plot(this, dataSpec, template, method);
-          }
-          case 'server': {
-            if (clients.vtkweb === undefined) {
-              clients.vtkweb = vtkweb.connect('@@@URL@@@');
-            }
-            return vtkweb.plot(this, dataSpec, template, method);
-          }
-          default: {
-            return Promise.reject(new Error('Invalid renderingType'));
-          }
+function init(el) {
+  const canvas = {
+    el,
+    clients: {},
+    plot(dataSpec, template, method, renderingType) {
+      switch (renderingType) {
+        case 'client': {
+          return plotly.plot(this, dataSpec, template, method);
         }
-      },
-      close() {
-        Object.keys(clients).map((k) => { return clients[k].close(this); });
-      },
-    };
-    return canvas;
-  },
-};
+        case 'server':
+          if (clients.vtkweb === undefined) {
+            clients.vtkweb = vtkweb.connect('@@@URL@@@/ws');
+          }
+          this.clients.vtkweb = clients.vtkweb;
+          return vtkweb.plot(this, dataSpec, template, method);
+        default:
+          return Promise.reject(new Error('Invalid renderingType'));
+      }
+    },
+    close() {
+      Object.keys(this.clients).map((k) => { return this.clients[k].then((c) => { c.close(this); }); });
+    },
+  };
+  return canvas;
+}
 
 export {
-  obj,
+  init,
   remoteRenderer,
 };
