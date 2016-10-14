c1c9f826ccbf44f6572095c1060ef1594617110f
Sam Fries <fries2@llnl.gov>
2016-10-14 11:47:47 -0700

    Refactored client logic into the backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import 'jquery';
-import createSession from './session';
+import vtkweb from './vtkweb';
+import plotly from './plotly';
 
-export {
-  createSession,
+
+const clients = {};
+
+const obj = {
+  init: (el) => {
+    const canvas = {
+      el,
+      plot(dataSpec, template, method, renderingType) {
+        switch (renderingType) {
+          case 'client': {
+            return plotly.plot(this, dataSpec, template, method);
+          }
+          case 'server': {
+            if (clients.vtkweb === undefined) {
+              clients.vtkweb = vtkweb.connect('@@@URL@@@');
+            }
+            return vtkweb.plot(this, dataSpec, template, method);
+          }
+          default: {
+            return Promise.reject(new Error('Invalid renderingType'));
+          }
+        }
+      },
+      close() {
+        Object.keys(clients).map((k) => { return clients[k].close(this); });
+      },
+    };
+    return canvas;
+  },
 };
+
+export { obj };
