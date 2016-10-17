9889ffebeb1aca4d9f68451177bc42f7b8e19c6d
Sam Fries <fries2@llnl.gov>
2016-10-17 10:35:32 -0700

    Removed unused functions

--- vcs_server/Visualizer.py
+++ vcs_server/Visualizer.py
@@ -17,36 +17,6 @@ class Visualizer(protocols.vtkWebProtocol):
 
     _active = {}
 
-    @register('cdat.vcs.templates')
-    def list_templates(self):
-        """Return a list of plot templates."""
-        return _templates
-
-    @register('cdat.vcs.methods')
-    def list_methods(self):
-        """Return a list of plot types and methods."""
-        return _methods
-
-    @register('cdat.view.variable')
-    def variable(self, plot, variable):
-        """Add or modify the variables used in the plot."""
-        if plot not in self._active:
-            return False
-        all_vars = []
-        for obj in variable:
-            f = FileLoader().get_reader(obj['file'])
-            all_vars.append(
-                f[obj['name']]
-            )
-        return self._active[plot].loadVariable(all_vars)
-
-    @register('cdat.view.template')
-    def template(self, plot, template):
-        """Change the given plot's template."""
-        if plot in self._active:
-            return self._active[plot].setTemplate(template)
-        return False
-
     @register('cdat.view.create')
     def create(self, variable, template, method, opts={}):
         try:
@@ -76,7 +46,7 @@ class Visualizer(protocols.vtkWebProtocol):
 
     @register('cdat.view.close')
     def remove_view(self, id):
-        print 'close window %s'%id
+        print 'close window %s' % id
         vis = self._active.pop(id)
         if vis:
             vis.close()
@@ -84,44 +54,44 @@ class Visualizer(protocols.vtkWebProtocol):
             return True
         return False
 
-    @classmethod
-    def detect_nvars(cls, g_type, g_method, g_obj):
-        """Try to return the number of variables required for the plot method.
 
-        Returns the number of variables required by the plot type.
-        This isn't really exposed by vcs, so this is written by following this
-        insanity:
-        https://github.com/UV-CDAT/uvcdat/blob/master/Packages/vcs/Lib/Canvas.py#L251
+def detect_nvars(cls, g_type, g_method, g_obj):
+    """Try to return the number of variables required for the plot method.
 
-        The reality is that this api will need to be more complicated in the
-        future to account some methods (like meshfill) that can take one or two
-        variables depending on the grid.
-        """
-        g_type = g_type.lower()
+    Returns the number of variables required by the plot type.
+    This isn't really exposed by vcs, so this is written by following this
+    insanity:
+    https://github.com/UV-CDAT/uvcdat/blob/master/Packages/vcs/Lib/Canvas.py#L251
 
-        # first check for g_naslabs
-        if hasattr(g_obj, 'g_nslabs'):
-            return g_obj.g_nslabs
+    The reality is that this api will need to be more complicated in the
+    future to account some methods (like meshfill) that can take one or two
+    variables depending on the grid.
+    """
+    g_type = g_type.lower()
 
-        # then check methods that require 2 variables
-        if g_type in _2d_methods:
-            return 2
+    # first check for g_naslabs
+    if hasattr(g_obj, 'g_nslabs'):
+        return g_obj.g_nslabs
 
-        # meshfill takes one or two, but there are extra requirements that we will
-        # have to pass through the api once they are understood
-        if g_type == 'meshfill':
-            return 1
+    # then check methods that require 2 variables
+    if g_type in _2d_methods:
+        return 2
 
-        # low level primitives should probably not happen
-        if g_type in _primitives:
-            return 0
-
-        # 1d takes 2 variables
-        if g_type == '1d':
-            return 2
-
-        # otherwise assume 1
+    # meshfill takes one or two, but there are extra requirements that we will
+    # have to pass through the api once they are understood
+    if g_type == 'meshfill':
         return 1
+
+    # low level primitives should probably not happen
+    if g_type in _primitives:
+        return 0
+
+    # 1d takes 2 variables
+    if g_type == '1d':
+        return 2
+
+    # otherwise assume 1
+    return 1
 
 
 # initialize the list of templates and graphics methods
