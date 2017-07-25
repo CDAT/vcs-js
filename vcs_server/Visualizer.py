a3053980cf257ebbb1c618d304595c89652da208
Dan Lipsa <dan.lipsa@kitware.com>
2017-07-25 10:25:14 -0400

    Revert detect_nvars to a class method.

--- vcs_server/Visualizer.py
+++ vcs_server/Visualizer.py
@@ -54,44 +54,44 @@ class Visualizer(protocols.vtkWebProtocol):
             return True
         return False
 
+    @classmethod
+    def detect_nvars(cls, g_type, g_method, g_obj):
+        """Try to return the number of variables required for the plot method.
 
-def detect_nvars(cls, g_type, g_method, g_obj):
-    """Try to return the number of variables required for the plot method.
+        Returns the number of variables required by the plot type.
+        This isn't really exposed by vcs, so this is written by following this
+        insanity:
+        https://github.com/UV-CDAT/uvcdat/blob/master/Packages/vcs/Lib/Canvas.py#L251
 
-    Returns the number of variables required by the plot type.
-    This isn't really exposed by vcs, so this is written by following this
-    insanity:
-    https://github.com/UV-CDAT/uvcdat/blob/master/Packages/vcs/Lib/Canvas.py#L251
+        The reality is that this api will need to be more complicated in the
+        future to account some methods (like meshfill) that can take one or two
+        variables depending on the grid.
+        """
+        g_type = g_type.lower()
 
-    The reality is that this api will need to be more complicated in the
-    future to account some methods (like meshfill) that can take one or two
-    variables depending on the grid.
-    """
-    g_type = g_type.lower()
+        # first check for g_naslabs
+        if hasattr(g_obj, 'g_nslabs'):
+            return g_obj.g_nslabs
 
-    # first check for g_naslabs
-    if hasattr(g_obj, 'g_nslabs'):
-        return g_obj.g_nslabs
+        # then check methods that require 2 variables
+        if g_type in _2d_methods:
+            return 2
 
-    # then check methods that require 2 variables
-    if g_type in _2d_methods:
-        return 2
+        # meshfill takes one or two, but there are extra requirements that we will
+        # have to pass through the api once they are understood
+        if g_type == 'meshfill':
+            return 1
 
-    # meshfill takes one or two, but there are extra requirements that we will
-    # have to pass through the api once they are understood
-    if g_type == 'meshfill':
+        # low level primitives should probably not happen
+        if g_type in _primitives:
+            return 0
+
+        # 1d takes 2 variables
+        if g_type == '1d':
+            return 2
+
+        # otherwise assume 1
         return 1
-
-    # low level primitives should probably not happen
-    if g_type in _primitives:
-        return 0
-
-    # 1d takes 2 variables
-    if g_type == '1d':
-        return 2
-
-    # otherwise assume 1
-    return 1
 
 
 # initialize the list of templates and graphics methods
