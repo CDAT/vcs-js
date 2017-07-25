6a0883de3dde5da963c1a056d6f0e57efc74dd58
Dan Lipsa <dan.lipsa@kitware.com>
2017-07-25 16:49:22 -0400

    Partial cherry-pick from
    commit e297eb21dfe1679d74ea77354ed791012278384a
    Author: Sam Fries <fries2@llnl.gov>
    Date:   Mon Oct 17 13:45:24 2016 -0700
    
        Migrated web server to tornado, made plotly use CDMS

--- demo/demo.js
+++ demo/demo.js
@@ -9,13 +9,13 @@ $(function () {
   var boxfill = {"fillareaopacity": [], "datawc_timeunits": "days since 2000", "projection": "linear", "xticlabels1": "*", "xticlabels2": "*", "ymtics1": "", "ymtics2": "", "datawc_x1": 1e+20, "datawc_x2": 1e+20, "boxfill_type": "linear", "xmtics1": "", "fillareacolors": null, "xmtics2": "", "color_2": 255, "datawc_calendar": 135441, "fillareaindices": [1], "color_1": 0, "colormap": null, "missing": [0.0, 0.0, 0.0, 100.0], "xaxisconvert": "linear", "level_2": 1e+20, "ext_1": false, "ext_2": false, "datawc_y2": 1e+20, "datawc_y1": 1e+20, "yaxisconvert": "linear", "legend": null, "name": "__boxfill_717978942019492", "yticlabels1": "*", "yticlabels2": "*", "fillareastyle": "solid", "levels": [1e+20, 1e+20], "g_name": "Gfb", "level_1": 1e+20};
   var vector = {"datawc_timeunits": "days since 2000", "projection": "linear", "reference": 1e+20, "xticlabels1": "*", "xticlabels2": "*", "linecolor": null, "ymtics1": "", "ymtics2": "", "linewidth": null, "datawc_x1": 1e+20, "datawc_x2": 1e+20, "xmtics1": "", "xmtics2": "", "datawc_calendar": 135441, "alignment": "center", "type": "arrows", "colormap": null, "xaxisconvert": "linear", "scale": 1.0, "linetype": null, "datawc_y2": 1e+20, "datawc_y1": 1e+20, "yaxisconvert": "linear", "name": "vector_full", "yticlabels1": "*", "yticlabels2": "*", "scalerange": [0.1, 1.0], "scaleoptions": ["off", "constant", "normalize", "linear", "constantNNormalize", "constantNLinear"], "g_name": "Gv", "scaletype": "constantNNormalize"};
   var vector_subview = {"datawc_timeunits": "days since 2000", "projection": "linear", "reference": 1e+20, "xticlabels1": "*", "xticlabels2": "*", "linecolor": null, "ymtics1": "", "ymtics2": "", "linewidth": null, "datawc_x1": 60, "datawc_x2": 180, "xmtics1": "", "xmtics2": "", "datawc_calendar": 135441, "alignment": "center", "type": "arrows", "colormap": null, "xaxisconvert": "linear", "scale": 1.0, "linetype": null, "datawc_y2": 90, "datawc_y1": 0, "yaxisconvert": "linear", "name": "subset_vector", "yticlabels1": "*", "yticlabels2": "*", "scalerange": [0.1, 1.0], "scaleoptions": ["off", "constant", "normalize", "linear", "constantNNormalize", "constantNLinear"], "g_name": "Gv", "scaletype": "constantNNormalize"};
+  var isofill = {"g_name": "Gfi"};
   var dv3d = {ScaleColormap: null, ScaleOpacity: null, BasemapOpacity: null, Camera: "{}", ZSlider: null, YSlider: null, ToggleVolumePlot: null, PointSize: null, Configure: null, XSlider: null, SliceThickness: null, axes: "xyz", plot_attributes: {name: "3d_scalar", template: "default"}, IsosurfaceValue: null, VerticalScaling: null, ChooseColormap: null, ToggleSurfacePlot: null, Colorbar: null, ncores: 8, ScaleTransferFunction: null, name: "default", ToggleClipping: null, Animation: null, g_name: "3d_scalar" };
 
-  var canvas = vcs.init(document.getElementById('vcs-isofill'));
-
-  // generate the plot when all of the promises resolve
   var dataSpec = variables.clt;
-  var rendererPromise = canvas.plot(dataSpec, 'default', boxfill, 'server');
+
+  var canvas = vcs.init(document.getElementById('vcs-isofill'));
+  var rendererPromise = canvas.plot(dataSpec, boxfill, 'default', 'server');
   rendererPromise.then((renderer) => {
     const imagePromise = new Promise((resolve, reject) => {
       renderer.onImageReady(resolve);
@@ -24,54 +24,26 @@ $(function () {
       console.log("Ready");
     });
   });
-                    
 
-   // generate another plot using client side rendering
-  // we don't have an api for getting data yet, so we'll
-  // just use an ajax request to data.kitware.com
-  var cltPromise = $.ajax('https://data.kitware.com/api/v1/file/576aa3c08d777f1ecd6701ae/download');
-  var latPromise = $.ajax('https://data.kitware.com/api/v1/item/576aa3c08d777f1ecd6701b0/download');
-  var lonPromise = $.ajax('https://data.kitware.com/api/v1/item/576aa3c08d777f1ecd6701b9/download');
   var canvas2 = vcs.init(document.getElementById('plotly-isofill'));
-
-  // This is all very rough, likely much of this should be wrapped inside the api
-  Promise.all([
-    cltPromise, latPromise, lonPromise
-  ]).then(function (arg) {
-    var clt = arg[0];
-    var lat = arg[1];
-    var lon = arg[2];
-    var timestep = 0;
-
-    var data = {
-      x: lon.data,
-      y: lat.data
-    };
-
-    function draw() {
-      data.z = ndarray(clt.data, clt.shape).pick(timestep, null, null);
-      canvas2.plot(data, 'default', { "g_type": "Gfi" }, 'client');
-      timestep = (timestep + 1) % clt.shape[0];
-    }
-    draw();
-  });
+  canvas2.plot(dataSpec, isofill, 'default', 'client');
 
   var canvas3 = vcs.init(document.getElementById('vcs-vector'));
     // generate the plot when all of the promises resolve
   var dataSpec = [variables.u, variables.v];
-  canvas3.plot(dataSpec, 'default', vector, 'server');
+  canvas3.plot(dataSpec, vector);
 
   var canvas4 = vcs.init(document.getElementById('vcs-vector-subset'));
 
   // generate the plot when all of the promises resolve
   var dataSpec = [variables.u, variables.v];
-  canvas4.plot(dataSpec, 'default', vector_subview, 'server');
+  canvas4.plot(dataSpec, vector_subview);
 
   var canvas5 = vcs.init(document.getElementById('vcs3d'));
 
   // generate the plot when all of the promises resolve
   var dataSpec = variables.airt;
-  canvas5.plot(dataSpec, 'default', dv3d, 'server');
+  canvas5.plot(dataSpec, dv3d, 'default', 'server');
 
   $(window).on('beforeunload', function() {
     canvas.close();
