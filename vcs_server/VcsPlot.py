fdd7de03b46683ba1cec2319c67c92e0ad938743
Dan Lipsa <dan.lipsa@kitware.com>
2017-08-17 11:04:26 -0400

    Revert "Fixed semi-syntax-error"
    This reverts commit cecea79d204059898d871c218df67539ee6b03e4.

--- vcs_server/VcsPlot.py
+++ vcs_server/VcsPlot.py
@@ -40,16 +40,16 @@ class VcsPlot(object):
         for k in gm:
             if k == "name":
                 continue
-            if gm[k] == 100000000000000000000:
+            if gm[k] == 100000000000000000000L:
                 gm[k] = 1e20
-            if gm[k] == -100000000000000000000:
+            if gm[k] == -100000000000000000000L:
                 gm[k] = -1e20
             if isinstance(gm[k], list):
                 conv = []
                 for v in gm[k]:
-                    if v == 100000000000000000000:
+                    if v == 100000000000000000000L:
                         conv.append(1e20)
-                    elif v == -100000000000000000000:
+                    elif v == -100000000000000000000L:
                         conv.append(-1e20)
                     else:
                         conv.append(v)
