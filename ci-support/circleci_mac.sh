6c33c054977e9898cc546711fc29a401b2b18ce6
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-01 09:31:31 -0700

    no test, just ls

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -4,7 +4,7 @@ echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
 echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
 echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
 #python run_tests.py -v2 -s
-#RESULT=$?
-RESULTS=0
+ls -l
+RESULT=$?
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
 exit $RESULT
