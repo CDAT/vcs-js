1c86e3aa1eb3ed0102fc41e2d72031d2338a157c
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-01 09:10:56 -0700

    first try

---
+++ ci-support/circleci_mac.sh
@@ -0,0 +1,10 @@
+export UVCDAT_ANONYMOUS_LOG=False
+export PATH=${HOME}/miniconda/bin:${PATH}
+echo "CIRCLE CI BRANCH:"$CIRCLE_BRANCH
+echo "CI_PULL_REQUESTS"$CI_PULL_REQUESTS
+echo "CI_PULL_REQUEST"$CI_PULL_REQUEST
+#python run_tests.py -v2 -s
+#RESULT=$?
+RESULTS=0
+if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then bash ./ci-support/conda_upload.sh ; fi
+exit $RESULT
